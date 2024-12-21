fdbf34862cfebb72bb68029f6df197305fda7eac
Brad Clawsie <brad@b7j0c.org>
2024-12-21 11:58:35 -0600

    add AESGCM

--- lib/GrokLOC/Crypt.pm
+++ lib/GrokLOC/Crypt.pm
@@ -10,9 +10,7 @@ our $VERSION   = '0.0.1';
 our $AUTHORITY = 'cpan:bclawsie';
 
 class IV {
-  Readonly::Scalar our $LEN           => 16;
-  Readonly::Scalar our $RAND_SIZE     => 500;
-  Readonly::Scalar our $RAND_STRENGTH => 1;
+  Readonly::Scalar our $LEN => 16;
   #<<V
   field $value :param :reader;
   #>>V
@@ -23,18 +21,14 @@ class IV {
   }
 
   sub rand ($self) {
-    use Crypt::Random         qw( makerandom );
-    use Crypt::Digest::SHA256 qw( sha256_b64 );
-    my $r = makerandom(Size => $RAND_SIZE, Strength => $RAND_STRENGTH);
-    my $s = substr unpack('H*', sha256_b64($r)), 0, $LEN;
+    use Crypt::Random qw( makerandom );
+    my $s = substr makerandom(Size => 64, Strength => 1), 0, $LEN;
     return $self->new(value => $s);
   }
 }
 
 class Key {
-  Readonly::Scalar our $LEN           => 32;
-  Readonly::Scalar our $RAND_SIZE     => 500;
-  Readonly::Scalar our $RAND_STRENGTH => 1;
+  Readonly::Scalar our $LEN => 32;
   #<<V
   field $value :param :reader;
   #>>V
@@ -45,11 +39,34 @@ class Key {
   }
 
   sub rand ($self) {
-    use Crypt::Random         qw( makerandom );
-    use Crypt::Digest::SHA256 qw( sha256_b64 );
-    my $r = makerandom(Size => $RAND_SIZE, Strength => $RAND_STRENGTH);
-    my $s = substr unpack('H*', sha256_b64($r)), 0, $LEN;
+    use Crypt::Random qw( makerandom );
+    my $s = substr makerandom(Size => 128, Strength => 1), 0, $LEN;
     return $self->new(value => $s);
+  }
+}
+
+class AESGCM {
+  Readonly::Scalar our $TAG_LEN => 32;
+
+  sub encrypt ($self, $pt, $key, $iv) {
+    use Crypt::AuthEnc::GCM ();
+    my $ae  = Crypt::AuthEnc::GCM->new('AES', $key, $iv);
+    my $ct  = unpack('H*', $ae->encrypt_add($pt));
+    my $tag = unpack('H*', $ae->encrypt_done());
+    return $iv . $tag . $ct;    # this is $e in decrypt
+  }
+
+  sub decrypt ($self, $e, $key) {
+    use Carp                qw( croak );
+    use Crypt::AuthEnc::GCM ();
+    my $iv      = substr $e, 0, $IV::LEN;
+    my $tag     = substr $e, $IV::LEN, $TAG_LEN;
+    my $ct      = substr $e, $IV::LEN + $TAG_LEN;
+    my $ae      = Crypt::AuthEnc::GCM->new('AES', $key, $iv);
+    my $pt      = $ae->decrypt_add(pack('H*', $ct));
+    my $tag_out = $ae->decrypt_done();
+    croak 'tag mismatch' unless unpack('H*', $tag_out) eq $tag;
+    return $pt;
   }
 }
 
