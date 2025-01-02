f0c7751c673756a2abec15b53ada86169ec5eb2d
Brad Clawsie <brad@b7j0c.org>
2025-01-02 11:39:45 -0600

    introduce Org test

--- lib/GrokLOC/App/Admin/Org.pm
+++ lib/GrokLOC/App/Admin/Org.pm
@@ -4,12 +4,25 @@ use strictures 2;
 use Object::Pad;
 use lib '../../../lib';
 use GrokLOC::Models;
+use GrokLOC::Safe;
 
 # ABSTRACT: Organization model support.
 
 our $VERSION   = '0.0.1';
 our $AUTHORITY = 'cpan:bclawsie';
 
-class Org :isa(Base) { }
+class Org :isa(Base) {
+  use Carp::Assert::More qw( assert_isa );
+
+  #<<V
+  field $name :param :reader;
+  field $owner :param :reader;
+  #>>V
+
+  ADJUST {
+    assert_isa($name,  'VarChar', 'name is not type VarChar');
+    assert_isa($owner, 'ID',      'owner is not type ID');
+  }
+}
 
 __END__
