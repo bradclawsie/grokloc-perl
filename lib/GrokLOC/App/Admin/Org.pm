06636ebebeed0f9f14979007ea91162c65156343
Brad Clawsie <brad@b7j0c.org>
2025-08-18 14:12:55 -0500

    work on org insert

--- lib/GrokLOC/App/Admin/Org.pm
+++ lib/GrokLOC/App/Admin/Org.pm
@@ -9,7 +9,7 @@ our $VERSION   = '0.0.1';
 our $AUTHORITY = 'cpan:bclawsie';
 
 class Org {
-  use Carp::Assert::More qw( assert_isa assert_nonblank );
+  use Carp::Assert::More qw( assert_is assert_isa assert_nonblank );
   use GrokLOC::Models;
   use GrokLOC::Safe;
 
@@ -45,20 +45,43 @@ class Org {
     );
   }
 
-  method insert ($txn,
+  method insert ($tx,
                  $owner_display_name,
                  $owner_email,
                  $owner_password,
                  $owner_key_version,
                  $version_key) {
-    assert_isa($txn, 'Mojo::Pg::Transaction',
-      'txn is not type Mojo::Pg::Transaction');
+    assert_isa($tx, 'Mojo::Pg::Transaction',
+      'tx is not type Mojo::Pg::Transaction');
     assert_isa($owner_display_name, 'VarChar',
       'owner_display_name not type VarChar');
     assert_isa($owner_email,    'VarChar',  'owner_email not type VarChar');
     assert_isa($owner_password, 'Password', 'owner_password not type Password');
     assert_nonblank($owner_key_version, 'owner_key_version malformed');
     assert_isa($version_key, 'VersionKey', 'version_key not type VersionKey');
+
+    # If $id is not $ID::NIL, then it is likely that this Org
+    # has already been inserted; the db generates $id.
+    assert_is($id, $ID::NIL, 'db generates id on insert');
+
+    my $q = <<~'INSERT_ORG';
+    insert into orgs 
+    (name, owner, role, schema_version, status)
+    values
+    ($1, $2, $3, $4, $5)
+    returning id, ctime, mtime, signature
+    INSERT_ORG
+
+    my $results = $tx->db->query(
+      $q,
+      $name,
+      $ID::NIL,    # Not known yet, see below.
+      $meta->role->value,
+      $meta->schema_version->value,
+      $meta->status->value
+    );
+
+    $tx->commit;
   }
 
   method TO_JSON {
