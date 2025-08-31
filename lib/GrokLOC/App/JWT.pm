5ed65d57af95d443e8c2dd96640f45865a1d5f99
Brad Clawsie <brad@b7j0c.org>
2025-08-31 11:35:20 -0500

    Data::Checks for JWT

--- lib/GrokLOC/App/JWT.pm
+++ lib/GrokLOC/App/JWT.pm
@@ -1,7 +1,11 @@
 package GrokLOC::App::JWT;
 use v5.42;
 use strictures 2;
+use Data::Checks qw( Isa NumGT Str StrEq StrMatch );
 use Object::Pad;
+use Object::Pad::FieldAttr::Checked;
+use Signature::Attribute::Checked;
+use Sublike::Extended qw( method sub );
 
 # ABSTRACT: JWT management.
 
@@ -9,36 +13,32 @@ our $VERSION   = '0.0.1';
 our $AUTHORITY = 'cpan:bclawsie';
 
 class JWT {
-  use Carp qw( croak );
-  use Carp::Assert::More
-    qw (assert assert_cmp assert_is assert_isa assert_like assert_positive_integer);
-  use Crypt::JWT qw( decode_jwt encode_jwt );
-  use Net::IP    qw( ip_is_ipv4 ip_is_ipv6 );
-  use Readonly   ();
+  use Carp               qw( croak );
+  use Carp::Assert::More qw ( assert assert_cmp );
+  use Crypt::JWT         qw( decode_jwt encode_jwt );
+  use Net::IP            qw( ip_is_ipv4 ip_is_ipv6 );
+  use Readonly           ();
   use overload '""' => \&TO_STRING, 'bool' => \&TO_BOOL, fallback => 0;
   use GrokLOC::Models::ID;
 
   Readonly::Scalar our $TOKEN_TYPE => 'Bearer';
 
-  field $exp :param : reader;
-  field $nbf :param : reader;
-  field $iss :param : reader;
-  field $sub :param : reader;
-  field $cip :param : reader;
+  field $exp :param : reader : Checked(NumGT(0));
+  field $nbf :param : reader : Checked(NumGT(0));
+  field $iss :param : reader : Checked(StrEq('GrokLOC.com'));
+  field $sub :param : reader : Checked(Isa('ID'));
+  field $cip :param : reader : Checked(StrMatch(qr/[.:]/x));
 
   ADJUST {
     my $now = time;
-    assert_positive_integer($exp, 'exp not posint');
     assert_cmp($exp, '>=', $now, 'exp expired');
-    assert_positive_integer($nbf, 'nbf not posint');
     assert_cmp($nbf, '<=', $now, 'nbf fail');
-    assert_is($iss, 'GrokLOC.com', 'iss is not GrokLOC.com');
-    assert_isa($sub, 'ID', 'sub is not type ID');
-    assert_like($cip, qr/[.:]/x, 'cip not ip string');
     assert(ip_is_ipv4($cip) || ip_is_ipv6($cip), 'cip not ip');
   }
 
-  sub decode ($self, $token, $signing_key) {
+  sub decode ($self,
+              $token :Checked(Str),
+              $signing_key :Checked(Str)) {
     my $token_fields = decode_jwt(token => $token, key => $signing_key);
     return $self->new(
       exp => $token_fields->{exp},
@@ -49,7 +49,7 @@ class JWT {
     );
   }
 
-  method encode ($signing_key) {
+  method encode ($signing_key :Checked(Str)) {
     return encode_jwt(
       payload => {
         exp => $exp,
@@ -63,14 +63,16 @@ class JWT {
     );
   }
 
-  sub from_header ($self, $header, $signing_key) {
+  sub from_header ($self,
+                   $header :Checked(Str),
+                   $signing_key :Checked(Str)) {
     if ($header =~ /^$TOKEN_TYPE\s+(\S+)\s*$/x) {
       return $self->decode($1, $signing_key);
     }
     croak 'no header value found';
   }
 
-  method to_header ($signing_key) {
+  method to_header ($signing_key :Checked(Str)) {
     return $TOKEN_TYPE . q{ } . $self->encode($signing_key);
   }
 
